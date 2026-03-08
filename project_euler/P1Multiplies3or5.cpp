7cd4767cc0a296c6f8210ee242c7047cd94b6375
xSaidanMka <135195002+Saidan1314@users.noreply.github.com>
2026-03-07 21:21:16 -0600

    Refactor code and improve formatting in P1Multiplies3or5.cpp

--- project_euler/P1Multiplies3or5.cpp
+++ project_euler/P1Multiplies3or5.cpp
@@ -25,7 +25,7 @@ which can be writtens as follows using the gaussian sum formula:
 3*(flor(n/3)*(floor(n/3)+1))/2 + 5*(flor((n-1)/5)*(floor((n-1)/5)+1))/2 - 15*(floor(n/15)*(floor(n/15)+1))/2 
 
 
-Note: Note that we use n-1 in the case of the summation of the multiplies of 5 because because the
+Note: Note that we use n-1 in the case of the summation of the multiplies of 5 because the
 problem ask for the sum of the multiples of 3 or 5 BELOW 1000, 5 is a divisor of 1000, so
 we dont have to count this iteration.
 
@@ -40,4 +40,5 @@ int main() {
     ll sum15 = 15*((n/15)*((n/15)+1))/2;
     cout << sum3 + sum5 - sum15;
     return 0;
+
 }
